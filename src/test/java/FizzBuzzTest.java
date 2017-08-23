2d9514908df94d827e5f0177f0f6c864b1ce6f13
kakawano <kakawano@server.fake>
2017-08-23 23:07:51 +0900

    Added test case

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -18,6 +18,20 @@ public class FizzBuzzTest {
     }
 
     @Test
+    public void should_return_Fizz_when_passed_6() {
+        String expected = "Fizz";
+        String actual = FizzBuzz.fizzbuzz(6);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void should_return_Fizz_when_passed_99() {
+        String expected = "Fizz";
+        String actual = FizzBuzz.fizzbuzz(99);
+        assertEquals(expected, actual);
+    }
+
+    @Test
     public void should_return_Buzz_when_passed_5() {
         String expected = "Buzz";
         String actual = FizzBuzz.fizzbuzz(5);
@@ -25,9 +39,37 @@ public class FizzBuzzTest {
     }
 
     @Test
+    public void should_return_Buzz_when_passed_10() {
+        String expected = "Buzz";
+        String actual = FizzBuzz.fizzbuzz(10);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void should_return_Buzz_when_passed_100() {
+        String expected = "Buzz";
+        String actual = FizzBuzz.fizzbuzz(100);
+        assertEquals(expected, actual);
+    }
+
+    @Test
     public void should_return_FizzBuzz_when_passed_15() {
         String expected = "FizzBuzz";
         String actual = FizzBuzz.fizzbuzz(15);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void should_return_FizzBuzz_when_passed_30() {
+        String expected = "FizzBuzz";
+        String actual = FizzBuzz.fizzbuzz(30);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void should_return_FizzBuzz_when_passed_90() {
+        String expected = "FizzBuzz";
+        String actual = FizzBuzz.fizzbuzz(90);
         assertEquals(expected, actual);
     }
 
@@ -42,13 +84,6 @@ public class FizzBuzzTest {
     public void should_return_empty_string_when_passed_upper_than_100() {
         String expected = "";
         String actual = FizzBuzz.fizzbuzz(101);
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void should_return_Buzz_when_passed_100() {
-        String expected = "Buzz";
-        String actual = FizzBuzz.fizzbuzz(100);
         assertEquals(expected, actual);
     }
 }
