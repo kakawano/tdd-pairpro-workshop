866d6f8e5dd07183e7452894c12a179f00b5bed4
kakawano <kakawano@server.fake>
2017-08-23 23:02:38 +0900

    Added test case

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -18,9 +18,37 @@ public class FizzBuzzTest {
     }
 
     @Test
+    public void should_return_Fizz_when_passed_6() {
+        String expected = "Fizz";
+        String actual = FizzBuzz.fizzbuzz(6);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void should_return_Fizz_when_passed_99() {
+        String expected = "Fizz";
+        String actual = FizzBuzz.fizzbuzz(99);
+        assertEquals(expected, actual);
+    }
+
+    @Test
     public void should_return_Buzz_when_passed_5() {
         String expected = "Buzz";
         String actual = FizzBuzz.fizzbuzz(5);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void should_return_Buzz_when_passed_10() {
+        String expected = "Buzz";
+        String actual = FizzBuzz.fizzbuzz(10);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void should_return_Buzz_when_passed_100() {
+        String expected = "Buzz";
+        String actual = FizzBuzz.fizzbuzz(100);
         assertEquals(expected, actual);
     }
 }
